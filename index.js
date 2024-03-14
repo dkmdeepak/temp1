02140e0300f7037af1deefae9241e5e7310e0f0b
Deepak KM <dkmdeepak8137@gmail.com>
2024-03-14 11:28:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const JSONserver=require('json-server')
+
+const MPserver=JSONserver.create()
+
+const router=JSONserver.router('db.json')
+
+const middleware=JSONserver.defaults()
+
+const PORT=3000 | process.env.PORT
+
+MPserver.use(middleware)
+
+MPserver.use(router)
+
+MPserver.listen(PORT,()=>{
+    console.log(`Server is Running at:${PORT}`)
+})
